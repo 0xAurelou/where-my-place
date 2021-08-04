e9fe3f1de2c7bc4e24f0b63159abece6ff130ab4
Aurelien I <aurelien.isak@epita.fr>
2021-08-04 23:12:04 +0200

    Navbar start to look better

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,36 +1,88 @@
 import React from "react";
 import Shot from "../Images/shot.png";
-import MyPlace from '../Images/Where-my-place.png'
+import Shotlogo from "../Images/Where-my-place.png";
 import MenuIcon from "@material-ui/icons/Menu";
-import TextField from "@material-ui/core/TextField";
-import Brightness3Icon from '@material-ui/icons/Brightness3';
-import '../Styles/Navbar.css';
+import AppBar from "@material-ui/core/AppBar";
+
+import IconButton from "@material-ui/core/IconButton";
+import Toolbar from "@material-ui/core/Toolbar";
+import { TextField } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "7px",
+  },
+  img: {
+    height: "60px",
+  },
+  search: {
+    padding: "10px",
+    width: "700px",
+    display: "center",
+  },
+
+  icon: {
+    paddingLeft: "80px",
+    alignItems: "right",
+  },
+}));
 
 function Navbar() {
+  const classes = useStyles();
+
   return (
     <div className="navbar">
-      <div className="navbar-menu-btn">
-        <MenuIcon></MenuIcon>
-      </div>
-      <span className="navbar-img">
-          
-        <img src={Shot} alt="shot-logo"></img>
-        <img src={MyPlace} alt="my-place-logo"></img>
-
-      </span>
-      <div className="navbar-searchbar">
-      <TextField id="standard-search" label="Search" type="search"/>
-      </div>
-      <div className="navbar-icon">
-          <div className="navbar-darkmode-btn">
-              <btn>
-              <Brightness3Icon></Brightness3Icon>
-              </btn>
-              <btn>
-                  <a className="navbar-conatct-btn">Contact</a>
-              </btn>
+      <AppBar position="fixed" className="navbar-appBar">
+        <Toolbar className="navbar-toolbarColor">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            className="navbar-menuButton"
+          >
+            <MenuIcon />
+          </IconButton>
+          <div>
+            <IconButton
+              variant="contained"
+              type="submit"
+              className="navbar-button"
+              //onClick={() => dispatch(toggleDarkTheme)}
+            >
+              <img src={Shot} className={classes.img} alt="navbar-logo"></img>
+              <img
+                src={Shotlogo}
+                className={classes.img}
+                alt="navbar-logo"
+              ></img>
+            </IconButton>
           </div>
-      </div>
+          <div className="navbar-search">
+            <TextField
+              id="filled-search"
+              className={classes.search}
+              label="Search"
+              type="search"
+              variant="filled"
+            />
+          </div>
+          <div>
+            <btn>
+              <Brightness4Icon
+                className={classes.icon}
+                OnClick={console.log("Floup")}
+              ></Brightness4Icon>
+            </btn>
+          </div>
+          <div>
+            <AccountCircleIcon className={classes.icon}></AccountCircleIcon>
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
